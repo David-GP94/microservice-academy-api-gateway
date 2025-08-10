b42f13ef22d05a70b597ce0547a168ddcddc5c2d
PROCHURGRUPOCSI\david.garcia <david.garcia@grupocsi.com>
2025-08-09 23:27:08 -0600

    Se agrega primer comit a api-gateway

---
+++ src/main/java/com/academy/microservice_academy_api_gateway/MicroserviceAcademyApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.academy.microservice_academy_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MicroserviceAcademyApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAcademyApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/microservice_academy_api_gateway/MicroserviceAcademyApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.microservice_academy_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAcademyApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
